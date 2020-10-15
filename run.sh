09d2b59f24899dae1de00e096a58bd8134b4cdf5
aparra001 <61757290+aparra001@users.noreply.github.com>
2020-10-15 21:04:57 +0000

    Update and rename run.example.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Miner account
+export ETH_WALLET=0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72
+export WORKER_NAME=ec2
+# Start mining!
+docker run --gpus all -e 0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72 -e ec2 -P -it ethminer:0.18.0
