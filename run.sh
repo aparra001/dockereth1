ec1bc5b5b0f244db9820e2028c7e68229c683f0d
aparra001 <61757290+aparra001@users.noreply.github.com>
2020-10-16 18:56:36 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Miner account
-export ETH_WALLET=0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72
+export ETH_WALLET=0x1a81AD816445c610170F498037c663704A7348e1
 export WORKER_NAME=ec1
 
 # Start mining!
-docker run --gpus all -e 0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72 -e ec1 -P -it ethminer:0.16.1
+docker run --gpus all -e 0x1a81AD816445c610170F498037c663704A7348e1 -e ec1 -P -it ethminer:0.16.1
